a94315daed5cc12fcb37a767556486f72c715a58
anonymous14386 <ndiramio@gmail.com>
2017-04-04 20:31:41 -0400

    Update smallcoder.py

--- smallcoder.py
+++ smallcoder.py
@@ -1,1 +1,15 @@
+input = raw_input('Please enter a string: ')
 
+length = len( input )
+
+end = length - 1
+
+count = length
+
+x = 1
+
+while (count > 0):
+
+        print (input[x])
+
+        count = count - 1
